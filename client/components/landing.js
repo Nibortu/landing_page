f2458b8d96c59ff87f7495bb147e04d99dc82ca2
Nibortu <hood_06@mail.ru>
2019-11-16 16:48:59 +0500

    new 16.11

--- client/components/landing.js
+++ client/components/landing.js
@@ -4,13 +4,31 @@ import { bindActionCreators } from 'redux'
 
 const Dummy = () => {
   return (
-    <div className="home">
-      <div className="parallax_background parallax-window" data-parallax="scroll" data-image-src="images/blog.jpg" data-speed="0.8" />
-      <div className="home_overlay" />
-      <div className="home_container d-flex flex-column align-items-center justify-content-center">
-        <div className="home_title"><h1>blog</h1></div>
+    <header className="header">
+      <div className="header_wrap d-flex flex-row align-items-center justify-content-center">
+        <div className="logo">
+          <a href="/dashboard"><img src="images/logo_1.png" alt="" />
+          </a>
+        </div>
+        <nav className="main_nav">
+          <ul className="d-flex flex-row align-items-center justify-content-center">
+            <li><a href="index.html">home</a></li>
+            <li className="active"><a href="about.html">about us</a></li>
+            <li><a href="classes.html">classes</a></li>
+            <li><a href="blog.html">blog</a></li>
+            <li><a href="contact.html">contact</a></li>
+          </ul>
+        </nav>
+        <div className="social header_social">
+          <ul className="d-flex flex-row align-items-center justify-content-start">
+            <li><a href="http://pinterest.com">pinterest<i className="fa fa-pinterest" aria-hidden="true" /></a></li>
+            <li><a href="http://facebook.com">facebook<i className="fa fa-facebook" aria-hidden="true" /></a></li>
+            <li><a href="http://twitter.com">twitter<i className="fa fa-twitter" aria-hidden="true" /></a></li>
+          </ul>
+        </div>
+        <div className="hamburger ml-auto"><i className="fa fa-bars" aria-hidden="true" /></div>
       </div>
-    </div>
+    </header>
   )
 }
 
