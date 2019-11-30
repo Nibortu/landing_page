a84eca1832f0a564bd3e6c87f3c2a2df215fe923
Nibortu <hood_06@mail.ru>
2019-11-30 15:01:02 +0500

    30.11

--- client/components/landing.js
+++ client/components/landing.js
@@ -96,6 +96,104 @@ const Dummy = () => {
           </div>
         </div>
       </section>
+
+      <section className="testimonials text-center bg-light">
+        <div className="container">
+          <h2 className="mb-5">What people are saying...</h2>
+          <div className="row">
+            <div className="col-lg-4">
+              <div className="testimonial-item mx-auto mb-5 mb-lg-0">
+                <img className="img-fluid rounded-circle mb-3" src="images/testimonials-1.jpg" alt="" />
+                <h5>Margaret E.</h5>
+                <p className="font-weight-light mb-0">&quot;This is fantastic! Thanks so much guys!&quot;</p>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="testimonial-item mx-auto mb-5 mb-lg-0">
+                <img className="img-fluid rounded-circle mb-3" src="images/testimonials-2.jpg" alt="" />
+                <h5>Fred S.</h5>
+                <p className="font-weight-light mb-0">&quot;Bootstrap is amazing. I&apos;ve been using it to create lots of super nice landing pages.&quot;</p>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="testimonial-item mx-auto mb-5 mb-lg-0">
+                <img className="img-fluid rounded-circle mb-3" src="images/testimonials-3.jpg" alt="" />
+                <h5>Sarah W.</h5>
+                <p className="font-weight-light mb-0">&quot;Thanks so much for making these free resources available to us!&quot;</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="call-to-action text-white text-center">
+        <div className="overlay" />
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-9 mx-auto">
+              <h2 className="mb-4">Ready to get started? Sign up now!</h2>
+            </div>
+            <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
+              <form>
+                <div className="form-row">
+                  <div className="col-12 col-md-9 mb-2 mb-md-0">
+                    <input type="email" className="form-control form-control-lg" placeholder="Enter your email..." />
+                  </div>
+                  <div className="col-12 col-md-3">
+                    <button type="submit" className="btn btn-block btn-lg btn-primary">Sign up!</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer className="footer bg-light">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 h-100 text-center text-lg-left my-auto">
+              <ul className="list-inline mb-2">
+                <li className="list-inline-item">
+                  <a href="#">About</a>
+                </li>
+                <li className="list-inline-item">&sdot;</li>
+                <li className="list-inline-item">
+                  <a href="#">Contact</a>
+                </li>
+                <li className="list-inline-item">&sdot;</li>
+                <li className="list-inline-item">
+                  <a href="#">Terms of Use</a>
+                </li>
+                <li className="list-inline-item">&sdot;</li>
+                <li className="list-inline-item">
+                  <a href="#">Privacy Policy</a>
+                </li>
+              </ul>
+              <p className="text-muted small mb-4 mb-lg-0">&copy; Your Website 2019. All Rights Reserved.</p>
+            </div>
+            <div className="col-lg-6 h-100 text-center text-lg-right my-auto">
+              <ul className="list-inline mb-0">
+                <li className="list-inline-item mr-3">
+                  <a href="#">
+                    <i className="fab fa-facebook fa-2x fa-fw" />
+                  </a>
+                </li>
+                <li className="list-inline-item mr-3">
+                  <a href="#">
+                    <i className="fab fa-twitter-square fa-2x fa-fw" />
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a href="#">
+                    <i className="fab fa-instagram fa-2x fa-fw" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
